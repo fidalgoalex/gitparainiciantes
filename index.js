bd7c83c684804fd703fe6d0aaccc005b40758479
fidalgoalex <fidalgo.alex1@gmail.com>
2018-11-07 14:16:00 -0200

    Nova rota adicionada

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
         message: 'Olá mundo esse é meu novo repositorio clonado.'})
 })
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({
+        message: 'Olá'})
+})
+
 app.listen(4001, () =>{
     console.log('Api inicializada na porta 4001');
 })
